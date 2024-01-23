2a39a020f4601c6ea3cb363f164b0e2d677f5102
jurchiks33 <jurisslisans@gmail.com>
2024-01-23 12:18:05 +0100

    log section

--- v0.1.py
+++ v0.1.py
@@ -20,6 +20,17 @@ stop_button = ttk.Button(main_frame, text="Stop")
 settings_button = ttk.Button(main_frame, text="Settings")
 exit_button = ttk.Button(main_frame, text="Exit", command=root.destroy)
 
+start_button.grid(row=1, column=0, padx=5, pady=5)
+stop_button.grid(row=1, column=1, padx=5, pady=5)
+settings_button.grid(row=1, column=2, padx=5, pady=5)
+exit_button.grid(row=1, column=3, padx=5, pady=5)
+
+#Log Section.
+log_label = ttk.Label(main_frame, text="Log Section", 
+                      background="black", foreground="white")
+log_label.grid(row=2, column=0, columnspan=4, 
+               sticky=(tk.W, tk.E, tk.N, tk.S), pady=5)
+
 
                         
 
