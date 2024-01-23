6ec18fef96f58cdb59552edf6444e773c6006e80
jurchiks33 <jurisslisans@gmail.com>
2024-01-23 11:58:20 +0100

    work on a buttons

--- v0.1.py
+++ v0.1.py
@@ -11,5 +11,15 @@ main_frame.grid(row=0, column=0, sticky=(tk.W,
                 tk.E, tk.N, tk.S))
 
 #Chart Area.
+chart_label = ttk.Label(main_frame, text="Chart Area", 
+                        background="gray", foreground="white")
 
+#Control Buttons.
+start_button = ttk.Button(main_frame, text="Start")
+stop_button = ttk.Button(main_frame, text="Stop")
+settings_button = ttk.Button(main_frame, text="Settings")
+exit_button = ttk.Button(main_frame, text="Exit", command=root.destroy)
+
+
+                        
 
