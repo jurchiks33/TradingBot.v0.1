88ab0e38824309511c67a38f7cb4153919d8dd1b
jurchiks33 <jurisslisans@gmail.com>
2024-01-24 13:09:13 +0100

    minor adjustements

--- v0.1.py
+++ v0.1.py
@@ -5,8 +5,8 @@ from data_scraper import fetch_crypto_data
 def main():
     #fetch data for Bitcoin
     btc_data = fetch_crypto_data('BTC-USD', period='5d', interval='1h')
-    print(btc_data.head())
-    window = GUI.create_main_window()
+
+    window = GUI.create_main_window(btc_data)
     window.mainloop()
 
 if __name__ == "__main__":
