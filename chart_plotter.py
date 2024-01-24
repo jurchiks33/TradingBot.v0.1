5e99fcea22f13486330a28d2d8b8612c1b58fedd
jurchiks33 <jurisslisans@gmail.com>
2024-01-24 13:00:11 +0100

    work on a display logic

--- chart_plotter.py
+++ chart_plotter.py
@@ -10,3 +10,13 @@ def create_chart_frame(parent, data, width, height):
     fig, axes = mpf.plot(data, type='candle', style=mpf_style,
                      returnfig=True) 
 
+    #Create canvas with figure in it.
+    canvas = FigureCanvasTkAgg(fig, master=parent)
+    canvas.draw()
+
+    #Place canvas in tkinter window.
+    chart_widget = canvas.get_tk_widget()
+    chart_widget.configure(width=width, height=height)
+    chart_widget.pack(side="top", fill="both", expand=True)
+
+    return canvas
