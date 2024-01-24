7b8ac1e93862e624097b0b43a903ab839a65f5a5
jurchiks33 <jurisslisans@gmail.com>
2024-01-24 12:40:05 +0100

    work on data chart

--- v0.1.py
+++ v0.1.py
@@ -1,7 +1,11 @@
 
 import GUI
+from data_scraper import fetch_crypto_data
 
 def main():
+    #fetch data for Bitcoin
+    btc_data = fetch_crypto_data('BTC-USD', period='5d', interval='1h')
+    print(btc_data.head())
     window = GUI.create_main_window()
     window.mainloop()
 
