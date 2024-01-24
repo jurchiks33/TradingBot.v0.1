485b06a56f2427678e966ab61d38d905167ef341
jurchiks33 <jurisslisans@gmail.com>
2024-01-24 12:35:40 +0100

    work on a chart

---
+++ data_scraper.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+
+def get_historical_data(ticker_symbol, period="id", intervals="1m"):
+    """
+    Fetches historical market data for the given cryptocurrency ticker symbol from Yahoo Finance.
+    
+    :param ticker_symbol: Cryptocurrency symbol, e.g., 'BTC-USD' for Bitcoin.
+    :param period: The data period to download (default is '1d').
+    :param interval: Data interval (default is '1m').
+    :return: DataFrame with historical market data.
+    """
