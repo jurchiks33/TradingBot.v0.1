e26e48744c7dbfc83c14b384fb2d0c606f5d8eef
jurchiks33 <jurisslisans@gmail.com>
2024-01-24 12:56:25 +0100

    work on a plotting

--- chart_plotter.py
+++ chart_plotter.py
@@ -1,3 +1,12 @@
 import mplfinance as mpf
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+def create_chart_frame(parent, data, width, height):
+    #Set style for the plot.
+    mpf_style = mpf.make_mpf_style(base_mpf_style='charles',
+                               rc={'font.size':8})
+
+    #create figure for the plot
+    fig, axes = mpf.plot(data, type='candle', style=mpf_style,
+                     returnfig=True) 
+
